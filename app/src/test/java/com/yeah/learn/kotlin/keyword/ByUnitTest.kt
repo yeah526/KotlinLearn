21f2ea1782d07e46b38ca6c2023a616c590f8dff
heweiyan <heweiyan526@163.com>
2020-10-06 13:59:45 +0800

    Finish unit test of keyword by usage.

--- app/src/test/java/com/yeah/learn/kotlin/keyword/ByUnitTest.kt
+++ app/src/test/java/com/yeah/learn/kotlin/keyword/ByUnitTest.kt
@@ -1,6 +1,7 @@
 package com.yeah.learn.kotlin.keyword
 
 import org.junit.Test
+import kotlin.properties.Delegates
 import kotlin.reflect.KProperty
 
 /**
@@ -68,22 +69,6 @@ class ByUnitTest {
 //    }
 
     /**
-     * 属性委托
-     */
-    @Test
-    fun testByValue() {
-        // 属性委托的本质
-        // 转成Java代码后会发现这个valueDelegate其实并不是一个String变量，而是替换成了ValueDelegate对象
-        // 因此看似是直接对String的取值和赋值操作，实际都被替换成了委托类的setValue()和getValue()方法
-        var valueDelegate: String? by ValueDelegate()
-        // 对String的取值操作，实际被替换成了ValueDelegate.getValue()方法
-        println("mValueDelegate = ${valueDelegate}")
-        // 对String的赋值操作，实际被替换成了ValueDelegate.setValue()方法
-        valueDelegate = "abc"
-        println("mValueDelegate = ${valueDelegate}")
-    }
-
-    /**
      * 属性委托类
      */
     class ValueDelegate<T> {
@@ -110,6 +95,29 @@ class ByUnitTest {
         }
     }
 
+    /**
+     * 属性委托
+     */
+    @Test
+    fun testByValue() {
+        // 属性委托的本质
+        // 转成Java代码后会发现这个valueDelegate其实并不是一个String变量，而是替换成了ValueDelegate对象
+        // 因此看似是直接对String的取值和赋值操作，实际都被替换成了委托类的setValue()和getValue()方法
+        var valueDelegate: String? by ValueDelegate()
+        // 对String的取值操作，实际被替换成了ValueDelegate.getValue()方法
+        println("mValueDelegate = ${valueDelegate}")
+        // 对String的赋值操作，实际被替换成了ValueDelegate.setValue()方法
+        valueDelegate = "abc"
+        println("mValueDelegate = ${valueDelegate}")
+    }
+
+    //    /**
+//     * lazy有三种mode
+//     * [LazyThreadSafetyMode.SYNCHRONIZED]：默认值，通过synchronized(lock)来保证同时只有一个线程能初始化
+//     * [LazyThreadSafetyMode.PUBLICATION]：多个线程会同时执行，初始化属性的函数会被多次调用，但是只有第一个返回的值被当做委托属性的值
+//     * [LazyThreadSafetyMode.NONE]：线程不安全，但是减少了线程同步的开销
+//     */
+//    val mLazyValue: String by lazy(LazyThreadSafetyMode.NONE) {
     val mLazyValue: String by lazy {
         println("Start init lazyValue.")
         "test lazy value"
@@ -122,6 +130,52 @@ class ByUnitTest {
     fun testByLazy() {
         println("Before init mLazyValue")
         println("lazyValue = $mLazyValue")
+    }
+
+    /**
+     * 测试可观察属性
+     */
+    @Test
+    fun testByObservable() {
+        var observableValue: String by Delegates.observable("initialValue") { property, oldValue, newValue ->
+            // 每次值的改变都会回调这里
+            println("property = ${property.name}, oldValue = $oldValue, newValue = $newValue")
+        }
+        println("Before change value, observableValue = $observableValue")
+        observableValue = "1"
+        println("after change value, observableValue = $observableValue")
+    }
+
+    /**
+     * 测试一个属性委托给另一个属性的类
+     */
+    class TestByOtherValueClazz {
+        /**
+         * 这个例子是：
+         * 如果类中有一个属性被更名了，那么就可以把旧属性的赋值和取值委托给新属性
+         * 这么做的好处就是不用修改原本引用旧属性的地方，也可以让那些地方使用新属性
+         * P.S：这里委托的写法是 ${对象}::${新属性名}
+         */
+        @Deprecated("Use 'mNewStringValue' instead", ReplaceWith("mNewStringValue"))
+        var mOldStringValue: String by this::mNewStringValue
+        var mNewStringValue: String = ""
+    }
+
+    /**
+     * 测试一个属性委托给另一个属性
+     */
+    @Test
+    fun TestByOtherValue() {
+        var testClazz = TestByOtherValueClazz()
+        testClazz.mOldStringValue = "I am old data."
+        println("mOldStringValue = ${testClazz.mOldStringValue}, mNewStringValue = ${testClazz.mNewStringValue}")
+        testClazz.mNewStringValue = "I am new data."
+        println("mOldStringValue = ${testClazz.mOldStringValue}, mNewStringValue = ${testClazz.mNewStringValue}")
+
+        // 局部变量也能委托给某个对象的变量，同理某个对象的变量也能委托给别的对象的变量
+        var tmpString: String by testClazz::mNewStringValue
+        tmpString = "I am temp data."
+        println("mOldStringValue = ${testClazz.mOldStringValue}, mNewStringValue = ${testClazz.mNewStringValue}, tmpString = $tmpString")
     }
 
 }
