00b1fdc73735f8626a2674d61eeecb8be1a1b7e1
heweiyan <heweiyan526@163.com>
2021-11-10 19:01:08 +0800

    Add higherOrderMethod1() AND higherOrderMethod2()

--- app/src/test/java/com/yeah/learn/kotlin/basic/HigherOrderMethodUnitTest.kt
+++ app/src/test/java/com/yeah/learn/kotlin/basic/HigherOrderMethodUnitTest.kt
@@ -4,7 +4,7 @@ import org.junit.Test
 
 /**
  * 高阶函数用法
- * 函数中的参数包含函数变量，则可以说这个函数是个高阶函数
+ * 如果函数中的参数或返回值带有函数，则可以说这个函数是个高阶函数
  *
  * @author: heweiyan
  * @date  : 2021/11/10
@@ -13,14 +13,29 @@ class HigherOrderMethodUnitTest {
 
     @Test
     fun testHigherOrderMethod() {
-        login("123", "123") {
-            println("Login result = $it")
+        higherOrderMethod1("123", "123") {
+            println("higherOrderMethod1 result = $it")
         }
+
+        println("higherOrderMethod2 result = ${higherOrderMethod2("123", "234").invoke()}")
     }
 
-    private fun login(userName: String, password: String, callback: (result: Boolean) -> Unit) {
-        println("Start login: userName = $userName; password = $password")
+    /**
+     * 参数带有函数
+     */
+    private fun higherOrderMethod1(
+        userName: String,
+        password: String,
+        callback: (result: Boolean) -> Unit
+    ) {
         callback(userName == password)
+    }
+
+    /**
+     * 返回值带有函数
+     */
+    private fun higherOrderMethod2(userName: String, password: String): () -> Boolean {
+        return { userName == password }
     }
 
 }
