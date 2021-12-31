5a612a762283c46a3287c74ff88adb068943d103
heweiyan <heweiyan526@163.com>
2021-12-31 16:24:03 +0800

    Add some annotations

--- app/src/test/java/com/yeah/learn/kotlin/buildinfunc/LetUnitTest.kt
+++ app/src/test/java/com/yeah/learn/kotlin/buildinfunc/LetUnitTest.kt
@@ -3,6 +3,8 @@ package com.yeah.learn.kotlin.buildinfunc
 import org.junit.Test
 
 /**
+ * let用法
+ *
  * @author: heweiyan
  * @date  : 2021/12/23
  */
@@ -13,6 +15,7 @@ class LetUnitTest {
         val mayNullString : String? = null
 //        val mayNullString : String? = "haha"
         val notNullString : String = mayNullString?.let {
+            // let函数的返回类型是看最后一行的类型
             it
         } ?: "mayNullString is null."
         println(notNullString)
